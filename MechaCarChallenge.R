7c58a677de10079b33880b23ed087c926e537307
Andrew Gonzales <andrewlaw.g@gmail.com>
2021-10-17 18:02:06 -0400

    deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -26,3 +26,19 @@ lot_summary <- suspension_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean 
                                                                            SD = sqrt(var(PSI)), .groups = 'keep')
 
 # Deliverable 3
+t.test(suspension_df$PSI, mu=1500)
+
+# Using subset() apart from t.test()
+lot1_df <- subset(suspension_df, Manufacturing_Lot == 'Lot1')
+lot2_df <- subset(suspension_df, Manufacturing_Lot == 'Lot2')
+lot3_df <- subset(suspension_df, Manufacturing_Lot == 'Lot3')
+
+t.test(lot1_df$PSI, mu=1500)
+t.test(lot2_df$PSI, mu=1500)
+t.test(lot3_df$PSI, mu=1500)
+
+# Attempting to use subset() within t.test() doesn't return different values despite my usage of subset()
+# It must be related to the use of 'x', however I cannot find documentation about it online
+t.test(x = suspension_df$PSI,
+       subset = Manufacturing_Lot == 'Lot2',
+       mu=1500)
